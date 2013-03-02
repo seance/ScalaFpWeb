a264b591d038c7db17f09ec5b88717dd40be671f
Jukka Viinamaki <jukka.viinamaki@futurice.com>
2013-03-02 12:33:02 +0200

    Added browser support and CORS rules

--- spray/src/main/scala/fpweb/Todos.scala
+++ spray/src/main/scala/fpweb/Todos.scala
@@ -1,9 +1,12 @@
 package fpweb
 
 import spray.routing.SimpleRoutingApp
+import spray.routing.RequestContext
 import spray.httpx.marshalling.Marshaller
 import spray.httpx.unmarshalling.Unmarshaller
 import spray.http._
+import spray.http.HttpMethods._
+import spray.http.HttpHeaders._
 import spray.http.StatusCodes._
 import spray.http.MediaRanges._
 import spray.http.ContentTypeRange._
@@ -18,40 +21,64 @@ object Todos extends App with SimpleRoutingApp with TodoJsonMarshalling {
   var todos = Map.empty[Int, Todo]
   
   startServer(interface = "localhost", port = 8080) {
-    path("todos") {
-      get {
-        produce(instanceOf[Iterable[Todo]]) { complete => ctx =>
-          complete(todos.values)
-        }
-      } ~
-      post {
-        entity(as[Todo]) { t =>
-          val id = idseq.getAndIncrement
-          todos = todos + (id -> t.copy(id = id))
-          complete(Created)
-        }
-      }
-    } ~
-    path("todos" / IntNumber) { id =>
-      get {
-        todos.get(id) map { t =>
-          produce(instanceOf[Todo]) { complete => ctx =>
-            complete(t)
+    allowCors {
+      browser ~
+      path("todos") {
+        get {
+          produce(instanceOf[Iterable[Todo]]) { complete => ctx =>
+            complete(todos.values)
           }
-        } getOrElse complete(NotFound)
+        } ~
+        post {
+          entity(as[Todo]) { t =>
+            val id = idseq.getAndIncrement
+            todos = todos + (id -> t.copy(id = id))
+            complete(Created)
+          }
+        }
       } ~
-      put {
-        entity(as[Todo]) { t =>
-          todos = todos + (id -> t.copy(id = id))
+      path("todos" / IntNumber) { id =>
+        get {
+          todos.get(id) map { t =>
+            produce(instanceOf[Todo]) { complete => ctx =>
+              complete(t)
+            }
+          } getOrElse complete(NotFound)
+        } ~
+        put {
+          entity(as[Todo]) { t =>
+            todos = todos + (id -> t.copy(id = id))
+            complete(OK)
+          }
+        } ~
+        delete {
+          todos = todos - id
           complete(OK)
         }
-      } ~
-      delete {
-        todos = todos - id
+      }
+    }
+  }
+  
+  def browser = {
+    (path("favicon.ico") & get) {
+      complete(NotFound)
+    } ~
+    method(OPTIONS) {
+      respondWithHeaders("Allow" -> "HEAD, GET, PUT, POST, DELETE, OPTIONS") {
         complete(OK)
       }
     }
   }
+  
+  def allowCors(f: RequestContext => Unit) = {
+    respondWithHeaders(
+        "Access-Control-Allow-Origin" -> "http://localhost:7000",
+        "Access-Control-Allow-Headers" -> "Origin, X-Requested-With, Content-Type, Accept",
+        "Access-Control-Allow-Methods" -> "HEAD, GET, PUT, POST, DELETE, OPTIONS"
+    )(f)
+  }
+  
+  implicit def header(t: Tuple2[String, String]): HttpHeader = RawHeader(t._1, t._2)
 }
 
 trait TodoJsonMarshalling extends DefaultJsonProtocol {
@@ -67,7 +94,7 @@ trait TodoJsonMarshalling extends DefaultJsonProtocol {
   }
   
   implicit val tum: Unmarshaller[Todo] = Unmarshaller[Todo](`application/*`) { 
-    case HttpBody(contentType, buffer) =>
+    case HttpBody(`application/json`, buffer) =>
       new String(buffer, UTF8).asJson.convertTo[Todo]
   }
 }
